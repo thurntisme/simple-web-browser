a3e2681f61a3b6c881a2cd924b8157da1f13fdd5
Sotatek-ThuyNguyen4 <thuy.nguyen4@sotatek.com>
2025-12-09 15:44:41 +0700

    init

---
+++ constants.py
@@ -0,0 +1,46 @@
+"""
+Constants for the application
+"""
+
+# Application settings
+APP_NAME = "Mozart"
+APP_VERSION = "2018.10"
+APP_ORGANIZATION = "HTOUKOUR COGNITIVE"
+APP_DOMAIN = "htoukour.co.za"
+APP_COPYRIGHT = "Copyright 2018 HTOUKOUR COGNITIVE"
+
+# Default browser settings
+DEFAULT_HOME_URL = "http://www.google.com"
+DEFAULT_PROTOCOL = "http"
+COMPANY_URL = "https://htoukour.co.za"
+
+# Window settings
+WINDOW_TITLE = "Mozart"
+TOOLBAR_ICON_SIZE = (16, 16)
+ABOUT_TITLE_FONT_SIZE = 20
+
+# Tab settings
+DEFAULT_TAB_LABEL = "Blank"
+DEFAULT_NEW_TAB_LABEL = "Homepage"
+MIN_TABS = 2
+
+# Image paths
+IMAGES_DIR = "images"
+ICON_BACK = "arrow-180.png"
+ICON_FORWARD = "arrow-000.png"
+ICON_RELOAD = "arrow-circle-315.png"
+ICON_HOME = "home.png"
+ICON_STOP = "cross-circle.png"
+ICON_NEW_TAB = "ui-tab--plus.png"
+ICON_OPEN_FILE = "disk--arrow.png"
+ICON_SAVE_FILE = "disk--pencil.png"
+ICON_PRINT = "printer.png"
+ICON_ABOUT = "question.png"
+ICON_HELP = "lifebuoy.png"
+ICON_SSL = "lock-ssl.png"
+ICON_NO_SSL = "lock-nossl.png"
+ICON_APP_64 = "ma-icon-64.png"
+ICON_APP_128 = "ma-icon-128.png"
+
+# File dialog filters
+HTML_FILE_FILTER = "Hypertext Markup Language (*.htm *.html);;All files (*.*)"
